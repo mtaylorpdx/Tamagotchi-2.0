e5c42685531ec7c6df22364908dcc68a66618b8f
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-11 12:01:06 -0700

    add Pet class with constructor and with find, get, delete methods

---
+++ Tamagotchi/Models/Pet.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace Tamagotchi.Models
+{
+  public class Pet
+  {
+    public string Name {get;set;}
+    public string Food {get;set;} = 10;
+    public string Attention {get;set;} = 10;
+    public string Rest {get;set;} = 10;
+    public int Id {get;}
+    private static List<Pet> _instances = new List<Pet> {};
+
+    public Pet(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Pet> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Pet Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    
+  }
+}
